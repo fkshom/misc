7c8dc4e9d1afa73a36f06ff88951e28e84a2b1c0
Shoma FUKUDA <fkshom@gmail.com>
2021-04-17 01:05:17 +0900

    wip

--- validator_py/src/validator_py/__init__.py
+++ validator_py/src/validator_py/__init__.py
@@ -1,41 +1,195 @@
 from pprint import pprint as pp
+import re
+import cerberus
+import ipaddress
 
-class CsvLinter:
-    pass
 
-class RuleValidator:
-    pass
+RULE_LABELS = "description,action,source_ip,destination_ip,source_port,destination_port,protocol,comment".split(',')
+
+decimal_type = cerberus.TypeDefinition('decimal', (str,), ())
+
+
+class AlignedCsvLinter:
+    def __init__(self):
+        self.errors = []
+        self._validators = []
+
+        self._validators.append(self._各カラムについてすべての行の幅が同一である)
+
+    def _error(self, lineno, msg):
+        pass
+
+    def _各カラムについてすべての行の幅が同一である(self, data):
+        expect_widths = [len(rawheader) for rawheader in data['rawheaders']]
+
+        for rawbodyline in rawbody:
+            values = rawbodyline.split(',')
+
+
+
+    def validate(self, f):
+        text = f.read()
+
+        rawlines = text.split("\n")
+        rawheaders = lines[0].split(',')
+        rawbody = lines[1:]
+
+        data = dict(
+            raw=text,
+            rawlines=rawlines,
+            rawheaders=rawheaders,
+            rawbody=rawbody
+        )
+
+        for _validator in self._validators:
+            _validator(data)
+
+
+
+
+class RuleValidator(cerberus.Validator):
+    def __init__(self):
+        super().__init__()
+        self.schema = {
+            "description": {
+                "type": 'string'
+            },
+            "action": {
+                "type": 'string',
+                "allowed": ["accept", "drop", ""]
+            },
+            "source_ip": {
+                "type": "string",
+                "check_with": "ipaddr"
+            },
+            "destination_ip": {
+                "type": "string",
+                "check_with": "ipaddr"
+            },
+            "source_port": {
+                "type": "string",
+                "regex": r"^\d+$|^\d+-\d+$"
+            },
+            "destination_port": {
+                "type": "string",
+                "regex": r"^\d+$|^\d+-\d+$"
+            },
+            "protocol": {
+                "type": "string",
+                "allowed": ["tcp", "ucp", "arp", "any"]
+            }
+        }
+        self.require_all = True
+        self.allow_unknown = False
+    
+    def _check_with_ipaddr(self, field, value):
+        if not re.fullmatch(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}|ANY", value):
+            self._error(field, f"'{field}' must be x.x.x.x/x or ANY. {value}")
+
+        try:
+            ipaddress.IPv4Interface(value)
+        except:
+            self._error(field, f"'{field}' cant be parsed by ipaddr.IPv4Interface. {value}")
+
 
 class Rule:
-    pass
+    def __init__(self, kwargs):
+        self.keys = kwargs.keys()
+        for key in self.keys:
+            setattr(self, key, kwargs[key])
+
+    def validate(self):
+        v = RuleValidator()
+        v.validate(self.__dir__)
+        if v.errors:
+            pp(v.errors)
+            raise ""
+
+    def to_dict(self):
+        result = {}
+        for key in self.keys:
+            result[key] = getattr(self, key)
+        return result
 
 class RuleStore:
     def __init__(self):
-        self.meta1 = None
+        self.dcname = None
+        self.pgname = None
         self.rules = []
+        self.errors = []
 
-    def _map_strip(self, arr):
-        return  map(lambda v: v.strip(), arr)
+    def _error(self, line_idx, msg):
+        self.errors.append(dict(
+            line_idx=line_idx,
+            message=msg,
+        ))
 
-    def load_from_string(self, text):
-        lines = text.split("\n")
+    def load(self, f):
+        lines = f.readlines()
+        line_cnt = 0
+    
+        # read meta area
         while True:
-            line = lines.pop(0)
-            if line.startswith("meta1: "):
-                self.meta1 = line.split(":")[1].strip()
+            line = lines.pop(0).rstrip()
+            line_cnt += 1
+            if line.strip() == '':
                 continue
-            if line.startswith("meta2: "):
-                self.meta2 = line.split(":")[1].strip()
-                continue
-            if line.startswith("---"):
+
+            if re.match(r'---+', line):
                 break
-            raise Exception(f"Unknown meta line: {line}")
 
-        reader = CsvReader(lines, Rule)
-        self.rules = reader.rows
-        headers = self._map_strip(lines.pop(0).split(","))
+            if line.startswith('dcname: '):
+                self.dcname = line.split(':', 2)[1].strip()
+                continue
+            elif line.startswith('pgname: '):
+                self.pgname = line.split(':', 2)[1].strip()
+            else:
+                raise Exception(f'Unknown meta data: {line}')
 
-    def save_to_string(self):
-        text = ""
-        writer = CsvWriter(self.rules, Rule)
-        writer
+        # read header
+        headers = list(map(lambda col: col.strip(), lines.pop(0).split(',')))
+        line_cnt += 1
+
+        # read data
+        for line_idx, line in enumerate(lines):
+            if line.strip() == '':
+                continue
+            values = list(map(lambda col: col.strip(), line.strip().split(',')))
+            if len(RULE_LABELS) != len(values):
+                self._error(line_cnt+line_idx, f"A number of columns is invalid. expect:{len(RULE_LABELS)}, actual:{len(values)}. Anyway continue.")
+
+            self.rules.append(Rule(dict(zip(headers, values))))
+
+
+    def save(self, f, format=True):
+        meta = []
+        meta.append(f'dcname: {self.dcname}' + "\n")
+        meta.append(f'pgname: {self.pgname}' + "\n")
+        meta.append(f'---' + "\n")
+
+        # prepare header
+        data = []
+        data.append(RULE_LABELS)
+
+        # prepate data
+        for rule in self.rules:
+            data.append([getattr(rule, key) for key in RULE_LABELS])
+
+        # calculate column size
+        data_t = list(zip(*data))
+        widths = list(map(lambda val: len(val), [max(cols, key=len) for cols in data_t]))
+
+        data2 = []
+        for row_num, row in enumerate(data):
+            new_row = []
+        
+            for val, width, col_name in zip(row, widths, RULE_LABELS):
+                val = val.ljust(width)
+                if col_name == 'comment':
+                    val = val.strip()
+                new_row.append(val)
+                        
+            line = ' , '.join(new_row) + "\n"
+            data2.append(line)
+
+        f.writelines(meta + data2)
--- validator_py/tests/validator_py/test_init.py
+++ validator_py/tests/validator_py/test_init.py
@@ -1,11 +1,159 @@
 import pytest
+from assertpy import assert_that
+from pprint import pprint as pp
+from unittest.mock import patch
 import validator_py
+import io
+import textwrap
 
-class Test1:
+
+class TestAlignedCsvLinter:
+    @pytest.fixture(scope='function', autouse=False)
+    def rulecsv1(self):
+        csv = (
+            "description , action , source_ip       , destination_ip  , source_port , destination_port , protocol , comment\n"
+            "abcd        ,        ,192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , cmt\n"
+            "efgh        ,       , 192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , \n"
+        )
+        yield csv
+
+    def test_1(self, rulecsv1):
+        rulecsvio = io.StringIO(rulecsv1)
+        linter = validator_py.AlignedCsvLinter()
+        linter.validate(rulecsvio)
+        assert len(linter.errors) == 2
+
+class TestRuleValidator:
     def test_1(self):
-        rulestore = validator_py.RuleStore()
-        rulestore.load_from_string()
-        rulestore.validate()
+        v = validator_py.RuleValidator()
+        v.validate({
+            "description": "",
+            "action": "accept",
+            "source_ip": '1.1.1.1/24',
+            "destination_ip": "2.2.2.2/24",
+            "source_port": "32768-65535",
+            "destination_port": "80",
+            "protocol": "tcp",
+        })
+        assert v.errors == {}
 
-        assert len(rulestore.errors) == 2
+    def test_1(self):
+        v = validator_py.RuleValidator()
+        v.validate({
+            "description": "",
+            "action": "test",
+            "source_ip": '1.-1.1.1/24',
+            "destination_ip": "2.2.2.2/33",
+            "source_port": "32768a",
+            "destination_port": "80udp",
+            "protocol": "a",
+        })
+        assert v.errors == {
+            'action': ['unallowed value test'],
+            'source_ip': ["'source_ip' cant be parsed by ipaddr.IPv4Interface. 1.-1.1.1/24", "'source_ip' must be x.x.x.x/x or ANY. 1.-1.1.1/24"],
+            'destination_ip': ["'destination_ip' cant be parsed by ipaddr.IPv4Interface. 2.2.2.2/33"],
+            'source_port': ["value does not match regex '^\\d+$|^\\d+-\\d+$'"],
+            'destination_port': ["value does not match regex '^\\d+$|^\\d+-\\d+$'"],
+            'protocol': ['unallowed value a'],
+        }
 
+
+class TestRuleStore:
+    @pytest.fixture(scope='function', autouse=False)
+    def rulecsv1(self):
+        rulecsv = textwrap.dedent("""
+        dcname: dc01
+        pgname: pg01
+        ---
+        description, action, source_ip, destination_ip, source_port, destination_port, protocol, comment
+        abcdf, , 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, cmt
+        efgh, , 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, 
+        """)[1:-1]
+
+        yield rulecsv
+
+    @pytest.fixture(scope='function', autouse=False)
+    def rulecsv2(self):
+        rulecsv = textwrap.dedent("""
+        dcname: dc01
+        pgname: pg01
+        ---
+        description, action, source_ip, destination_ip, source_port, destination_port, protocol, comment
+        abcdf, , 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, cmt
+        
+        efgh, , 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, 
+        """)[1:-1]
+
+        yield rulecsv
+
+    def test_1(self, rulecsv1):
+        rulecsvio = io.StringIO(rulecsv1)
+        store = validator_py.RuleStore()
+        store.load(rulecsvio)
+        assert store.dcname == 'dc01'
+        assert store.pgname == 'pg01'
+
+        assert store.rules[0].to_dict() == dict(
+            description='abcdf',
+            source_ip='192.168.11.1/32',
+            destination_ip='192.168.12.1/32',
+            source_port='ANY',
+            destination_port='ANY',
+            protocol='ANY',
+            action='',
+            comment='cmt'
+        )
+
+        store.rules[0].description = 'abcd'
+
+        rulecsvoutio = io.StringIO()
+        store.save(rulecsvoutio, format=True)
+        actual = rulecsvoutio.getvalue()
+        expect = (
+            "dcname: dc01\n"
+            "pgname: pg01\n"
+            "---\n"
+            "description , action , source_ip       , destination_ip  , source_port , destination_port , protocol , comment\n"
+            "abcd        ,        , 192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , cmt\n"
+            "efgh        ,        , 192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , \n"
+        )
+        assert actual == expect
+
+    def test_2(self, rulecsv2):
+        rulecsvio = io.StringIO(rulecsv2)
+        store = validator_py.RuleStore()
+        store.load(rulecsvio)
+        assert len(store.rules) == 2
+
+        store.rules[0].description = 'abcd'
+
+        rulecsvoutio = io.StringIO()
+        store.save(rulecsvoutio, format=True)
+        actual = rulecsvoutio.getvalue()
+        expect = (
+            "dcname: dc01\n"
+            "pgname: pg01\n"
+            "---\n"
+            "description , action , source_ip       , destination_ip  , source_port , destination_port , protocol , comment\n"
+            "abcd        ,        , 192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , cmt\n"
+            "efgh        ,        , 192.168.11.1/32 , 192.168.12.1/32 , ANY         , ANY              , ANY      , \n"
+        )
+        assert actual == expect
+
+    def test_3(self):
+        rulecsv = textwrap.dedent("""
+        dcname: dc01
+        pgname: pg01
+        ---
+        description, action, source_ip, destination_ip, source_port, destination_port, protocol, comment
+        abcdf, ,,,,, 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, cmt
+
+        efgh, , 192.168.11.1/32, 192.168.12.1/32, ANY, ANY, ANY, 
+        """)[1:-1]
+        rulecsvio = io.StringIO(rulecsv)
+        store = validator_py.RuleStore()
+        store.load(rulecsvio)
+        assert len(store.rules) == 2
+
+        assert len(store.errors) == 1
+        assert store.errors[0] == {'line_idx': 4, 'message': 'A number of columns is invalid. expect:8, actual:12. Anyway continue.'}
